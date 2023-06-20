8f3aa22b9b7b64465b1846d6f20b651bc7dfcb81
Senchxa <dawedik902@gmail.com>
2023-06-20 09:32:44 +0200

    check the norm

--- get_next_line.h
+++ get_next_line.h
@@ -13,13 +13,18 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
 # include <stdlib.h>
 # include <unistd.h>
+#include <stdio.h>
 
 char	*get_next_line(int fd);
-int		ft_strlen(const char *str);
-char	*ft_strchr(const char *s, int i);
-char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int i);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
